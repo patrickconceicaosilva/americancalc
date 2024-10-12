38b19947595c9ab23361f6a81d8fdbb7e990cb3b
Patrick da Conceição Silva <151814907+patrickconceicaosilva@users.noreply.github.com>
2024-10-11 22:53:39 -0300

    Update Code
    Update:
    Delivery fee price selector;
    Changing input from box type to number;

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const prices = {
     zipburguer: 10.50,
     xfrango: 14.50,
     gringo: 13.00,
-    entrega: 3.00
+    entrega3: 3.00,
+    entrega5: 5.00,
+    entrega10: 10.00
 }
 
 const valorTotal = document.getElementById("valortotal");
@@ -11,30 +13,20 @@ const valorTotal = document.getElementById("valortotal");
 function calculate() {
     let total = 0;
 
-    if (document.getElementById("xburguer").checked) {
-        total += prices.xburguer;
-    }
+    total += prices.xburguer * parseInt(document.getElementById("xburguer").value) || 0;
+    total += prices.zipburguer * parseInt(document.getElementById("zipburguer").value) || 0;
+    total += prices.xfrango * parseInt(document.getElementById("xfrango").value) || 0;
+    total += prices.gringo * parseInt(document.getElementById("gringo").value) || 0;
 
-    if (document.getElementById("zipburguer").checked) {
-        total += prices.zipburguer;
-    }
+    const entrega = document.querySelector('input[name="entrega"]:checked').value;
+    total += parseFloat(entrega);
 
-    if (document.getElementById("xfrango").checked) {
-        total += prices.xfrango;
-    }
-
-    if (document.getElementById("gringo").checked) {
-        total += prices.gringo;
-    }
-
-    if (document.getElementById("entrega").checked) {
-        total += prices.entrega;
-    }
-    
     valorTotal.textContent = total.toFixed(2).replace(".", ",");
+
 }
 
+calculate();
 
-document.querySelectorAll('#inputs input[type="checkbox"]').forEach(checkbox => {
-    checkbox.addEventListener('change', calculate);
-})
+document.querySelectorAll('#inputs input[type="number"], #inputs input[name="entrega"]').forEach(input => {
+    input.addEventListener("change", calculate);
+});
